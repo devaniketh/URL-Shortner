93b137517be8161f0b534d4d6898ed17239464ca
Aniketh <techrunstheworld@gmail.com>
2025-02-07 15:54:10 +0530

    feat: db-setup⚡

---
+++ shortylinker/src/models/Url.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document,Model, Schema} from 'mongoose'
+import { unique } from 'next/dist/build/utils'
+
+const urlSchema = new mongoose.Schema({
+    originalUrl: {
+        type : String,
+        required: true,
+        unique: true
+    },
+    shortUrl: {
+        type: String,
+        require: true,
+        unique : true
+
+    }
+}, {
+    timestamps:true
+});
+
+export interface IUrl extends Document{
+    originalUrl: string,
+    shortUrl: string
+}
+
+const Url: Model<IUrl> = mongoose.models.Url || mongoose.model<IUrl>('Url', urlSchema);
+
+export default Url;
